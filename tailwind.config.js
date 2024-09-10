71aef4978f3a90483a0cf37b2ddf1650b7845f6d
Norlys Castañeda <castaneda30@hotmail.com>
2024-09-09 19:51:17 -0500

    ajustes y configuración

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#121212',      // Color oscuro para el fondo
+        darkGray: '#1E1E1E',  // Gris oscuro para elementos secundarios
+        lightGray: '#F1F1F1', // Gris claro para el fondo
+        hover: '#8C1D2E',     // Color para hover
+      },
+    },
+  },
+  plugins: [],
+}
