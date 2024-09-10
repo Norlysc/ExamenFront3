2aec09e49a78332c03b6ad29f9a3d048d1cc7906
Norlys Castañeda <castaneda30@hotmail.com>
2024-09-09 19:51:46 -0500

    ajustes de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Routes/Home';
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import { ContextProvider, useAppContext } from './Components/utils/global.context';
 
 function App() {
+  const { state } = useAppContext(); // Obtener el estado del contexto global
+
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <Router>
+      <ContextProvider>
+        <div className={`${state.theme === 'dark' ? 'bg-dark text-white' : 'bg-lightGray text-black'}`}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/dentist/:id" element={<Detail />} />
+            <Route path="/favs" element={<Favs />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ContextProvider>
+    </Router>
   );
 }
 
